f81918607e2b744f5b66cc81b25870604c191aab
Sali Dev <sali.conceito@gmail.com>
2023-11-13 11:27:35 -0300

    mensagem

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex">
+      <div className='flex w-full h-screen bg-[#713BB3] justify-center items-center'>
+        <h1 className='text-white'>Site em construção</h1>
+      </div>
+    </main>
+  )
+}
