582a4622d3c6ec9ecd7d0918015ca9c98449525c
Sali Dev <sali.conceito@gmail.com>
2023-11-13 11:51:01 -0300

    Update de Site em construção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex">
-      <div className='flex w-full h-screen bg-[#713BB3] justify-center items-center'>
+      <div className='flex w-full h-[95vh] bg-[#713BB3] justify-center items-center'>
         <h1 className='text-white'>Site em construção</h1>
       </div>
     </main>
---
+++ src/components/Footer/footer.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Footer() {
+  return (
+    <div className='flex h-[5vh] w-full bg-black items-center justify-center'>
+      <p className='text-white text-[.8rem]'>© 2023 Digital Maker. Todos os direitos reservados.</p>
+
+    </div>
+  )
+}
