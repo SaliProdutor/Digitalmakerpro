340124209c2d62ab5fe869c1a9daee028b3c91ba
Sali Dev <sali.conceito@gmail.com>
2023-11-17 15:47:30 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/footer'
 import Header from '@/components/header'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin']})
 
 
 export const metadata: Metadata = {
   title: 'Digital Maker Pro',
-  description: 'Na Digital Maker, não apenas oferecemos serviços de marketing; oferecemos uma parceria estratégica para o crescimento personalizado da sua marca. Seja qual for a sua visão, estamos aqui para transformá-la em realidade.',
+  description: 'Na Digital Maker, não só oferecemos marketing, mas uma parceria estratégica para o crescimento personalizado da sua marca, transformando sua visão em realidade',
+}
+export const viewport: Viewport = {
+  themeColor: '#713BB3',
+  width: '1024'
 }
 
 export default function RootLayout({
@@ -21,9 +24,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-purple-white`}>
-        <Head>
-          <meta name="theme-color" content="#713BB3" />
-        </Head>
         <Header/>
         {children}
         <Footer/>
