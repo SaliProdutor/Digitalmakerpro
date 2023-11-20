2b699ba3a380b6ac886b43a2e8547257b778d833
Sali Dev <sali.conceito@gmail.com>
2023-11-20 10:24:35 -0300

    Correção de proporção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
               oferecemos uma parceria estratégica para o crescimento personalizado 
               da sua marca. Seja qual for a sua visão, estamos aqui para transformá-la em realidade.</p>
             <div className="flex flex-row gap-4 pt-12">
-              <a href='/#solucoes' className="bg-green-primary md:hover:bg-green-primary py-2 px-4 rounded-full text-white md:hover:text-white md:hover:border-white border-green-primary border-[1px]">Soluções</a>
+              <a href='/#solucoes' className="bg-green-primary md:hover:bg-green-primary py-2 px-4 rounded-full text-black md:hover:text-white md:hover:border-white border-black border-[1px]">Soluções</a>
               <a href='/#sobre' className="bg-green-transparent md:hover:bg-green-primary py-2 px-4 rounded-full text-white md:hover:text-white md:hover:border-white border-white border-[1px]">Sobre</a>
             </div>
         </div>
