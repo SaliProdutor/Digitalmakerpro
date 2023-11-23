e2f91d18fcde957b972259f35cc2ffcba3e93d27
SaliProdutor <sali.conceito@gmail.com>
2023-11-22 23:04:53 -0300

    teste de animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,126 @@
 "use client"
 import { CardFeedback } from "@/components/cardFeedback";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
 import { FaBehance, FaInstagram, FaCode, FaCamera, FaFilm, FaPenNib } from 'react-icons/fa';
 import { FaMeta } from "react-icons/fa6";
 import { SiAnalogue } from "react-icons/si";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 export default function Home() {
+ useLayoutEffect(()=>{
+  gsap.fromTo('.animation', {
+    opacity: 0,
+    y: 100
+  }, {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    ease: "power4.out"
+  })
+
+  return (()=>{
+    gsap.getTweensOf(".animation");
+  })
+ },[])
+
+ 
+ useLayoutEffect(()=>{
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.fromTo('.cardAnimation', {
+    opacity: 0,
+    y: 100
+  },{
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    ease: "power4.out",
+    scrollTrigger: {
+      trigger: '#sobre',
+      start: '30% center',
+      end: '50%',
+      scrub: true,
+    }
+  })
+
+  return (() => {
+    gsap.getTweensOf('.cardAnimation');
+  })
+},[])
+
+useLayoutEffect(()=>{
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.fromTo('.nomeAnimation', {
+    opacity: 0,
+    x: 100
+  },{
+    opacity: 1,
+    x: 0,
+    duration: 1,
+    delay: .3,
+    ease: "power4.out",
+    scrollTrigger: {
+      trigger: '#sobre',
+      start: '30% center',
+      end: '50%',
+      scrub: true,
+    }
+  })
+
+  return (() => {
+    gsap.getTweensOf('.nomeAnimation');
+  })
+},[])
+useLayoutEffect(()=>{
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.fromTo('.sobreAnimation', {
+    opacity: 0,
+    x: 100
+  },{
+    opacity: 1,
+    x: 0,
+    duration: 1,
+    delay: 8,
+    ease: "power4.out",
+    scrollTrigger: {
+      trigger: '#sobre',
+      start: '30% center',
+      end: '50%',
+      scrub: true,
+    }
+  })
+
+  return (() => {
+    gsap.getTweensOf('.sobreAnimation');
+  })
+},[])
+useLayoutEffect(()=>{
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.fromTo('.btnAnimation', {
+    opacity: 0,
+    x: 100
+  },{
+    opacity: 1,
+    x: 0,
+    duration: 1,
+    delay: 1,
+    ease: "power4.out",
+    scrollTrigger: {
+      trigger: '#sobre',
+      start: '30% center',
+      end: '50%',
+      scrub: true,
+    }
+  })
+
+  return (() => {
+    gsap.getTweensOf('.btnAnimation');
+  })
+},[])
+
+
   useEffect(() => {
     // Verifica se há um fragmento na URL
     const hash = window.location.hash;
@@ -29,9 +143,9 @@ export default function Home() {
       {/* Banner topo */}
       <div id="topo" data-section="topo" className='container flex justify-center items-center pt-[5rem] py-16 md:py-12 lg:py-32 bg-gradient-to-bl from-purple-primary to-purple-secundary md:min-w-[100vw] w-screen h-screen'>
         <div className="sm:w-[95vw] md:max-w-[64rem] p-8">
-          <h1 className='text-4xl md:text-6xl lg:text-7xl text-green-primary font-bold'>SUA MARCA</h1>
-          <h2 className='text-white font-light text-xl md:text-2xl lg:text:4xl'>nossas soluções personalizadas</h2>
-            <p className='max-w-[32rem] pt-8 text-md text-muted-foreground'>
+          <h1 className='animation opacity-0 text-4xl md:text-6xl lg:text-7xl text-green-primary font-bold'>SUA MARCA</h1>
+          <h2 className='animation opacity-0 text-white font-light text-xl md:text-2xl lg:text:4xl'>nossas soluções personalizadas</h2>
+            <p className='animation opacity-0 max-w-[32rem] pt-8 text-md text-muted-foreground'>
               Na Digital Maker, não apenas oferecemos serviços de marketing; 
               oferecemos uma parceria estratégica para o crescimento personalizado 
               da sua marca. Seja qual for a sua visão, estamos aqui para transformá-la em realidade.</p>
@@ -58,32 +172,32 @@ export default function Home() {
                   blurDataURL="/sobre-sali.jpg"
                   className="aspect-square rotate-3 rounded-2xl bg-white object-cover overflow-hidden scale-[.8]"
                 />
-                <div className="absolute top-16 -left-4 md-left-0 md:top-16 ml-2 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
+                <div className="cardAnimation absolute top-16 -left-4 md-left-0 md:top-16 ml-2 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
                   <FaCode/>
                   <p>Dev</p>
                 </div>
-                <div className="absolute top-16 -right-6 md:top-20 md:right-2 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
+                <div className="cardAnimation absolute top-16 -right-6 md:top-20 md:right-2 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
                   <FaFilm/>
                   <p>Filmaker</p>
                 </div>
-                <div className="absolute bottom-24 -right-4 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
+                <div className="cardAnimation absolute bottom-24 -right-4 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
                   <FaCamera/>
                   <p>Fotógrafo</p>
                 </div>
-                <div className="absolute bottom-24 md:bottom-32 -left-4 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
+                <div className="cardAnimation absolute bottom-24 md:bottom-32 -left-4 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
                   <FaPenNib/>
                   <p>Design</p>
                 </div>
-                <div className="absolute bottom-0 right-0 mr-32 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
+                <div className="cardAnimation absolute bottom-0 right-0 mr-32 px-8 py-4 z-[2] bg-white rounded-2xl flex flex-row justify-center items-center gap-2 scale-[.7]">
                   <SiAnalogue />
                   <p>Gestor</p>
                 </div>
               </div>
             </div>
             <div className="lg:order-first sm:px-2 md:pt-8 lg:pt-12">
-              <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white">Sali Produtor</h1>
-              <p className="mt-6 text-base text-dark-muted-foreground">Como é conhecido <span className="font-medium">Sali Produtor</span>, representa a essência da Digital Maker Pro, uma empresa que vai além do convencional, oferecendo soluções digitais verdadeiramente personalizadas. Sua abordagem centrada no cliente e sua habilidade em fornecer exatamente o que cada cliente precisa para atingir seus objetivos fazem da Digital Maker Pro a escolha ideal para empresas que buscam uma presença online impactante e eficaz.</p>
-              <div className='mt-6 flex justify-center md:justify-start gap-6'>
+              <h1 className="nomeAnimation text-3xl md:text-4xl lg:text-5xl font-bold text-white">Sali Produtor</h1>
+              <p className="sobreAnimation mt-6 text-base text-dark-muted-foreground">Como é conhecido <span className="font-medium">Sali Produtor</span>, representa a essência da Digital Maker Pro, uma empresa que vai além do convencional, oferecendo soluções digitais verdadeiramente personalizadas. Sua abordagem centrada no cliente e sua habilidade em fornecer exatamente o que cada cliente precisa para atingir seus objetivos fazem da Digital Maker Pro a escolha ideal para empresas que buscam uma presença online impactante e eficaz.</p>
+              <div className='btnAnimation mt-6 flex justify-center md:justify-start gap-6'>
                 <a className='opacity-[.8] text-white text-2xl hover:opacity-[1] transition-2000' aria-label="Nosso perfil na Behance" href="https://www.behance.net/sali_produtor" target="_blank" rel="noopener noreferrer">
                   <FaBehance/>
                 </a>
