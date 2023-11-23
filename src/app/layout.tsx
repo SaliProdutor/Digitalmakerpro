8590ee3fc09efef19b80b48acf32b71aa5004995
Sali Dev <sali.conceito@gmail.com>
2023-11-23 09:44:12 -0300

    Meta Icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={`${inter.className} bg-purple-white`}>
         <Header/>
         {children}
